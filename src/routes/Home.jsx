f0c859372611e3ad1a3da8935c4cd252af977cac
IceManbrz <81310383+IceManbrz@users.noreply.github.com>
2023-09-03 14:11:45 +0700

    update env

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { json } from "react-router-dom";
-
+const apiKey = process.env.REACT_APP_API_HOST;
 const Home = () =>{
     const [name, setName] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    console.log(apiKey);
 
 useEffect(() =>{
   setLoading(false);
-    fetch('http://universities.hipolabs.com/search?country=Indonesia')
+    fetch (apiKey)
     .then((response) => response.json()) 
   //.then((json) => console.log(json))
   .then((json) => setName((json)));
